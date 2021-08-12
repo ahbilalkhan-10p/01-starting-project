cc1aaa09dfe32772856d1890933b9f90b3307afc
ahbilalkhan-10p <ahbilalkhan@gmail.com>
2021-08-12 15:30:21 +0500

    lint

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -17,7 +17,6 @@ const CourseInput = props => {
   const formSubmitHandler = event => {
     event.preventDefault();
     if (enteredValue.trim().length === 0) {
-
       setIsValid(false);
       return;
     }
